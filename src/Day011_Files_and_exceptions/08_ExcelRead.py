454b35dac1de16a69f3316ecbc3467f192d47ff5
bamboodew <mrbamboodew@gmail.com>
2019-11-09 20:34:31 +0800

    PythonExcel_ReadDateAndMergeCells

--- src/Day011_Files_and_exceptions/08_ExcelRead.py
+++ src/Day011_Files_and_exceptions/08_ExcelRead.py
@@ -1,13 +1,13 @@
 import xlrd
 
-# from datetime import date, datetime
+from datetime import date, datetime
 
 file = 'test3.xls'
 
 
 def read_excel():
-    # 打开文件
-    wb = xlrd.open_workbook(filename=file)
+    # 打开文件;读取文件的时候需要将formatting_info参数设置为True，默认是False
+    wb = xlrd.open_workbook(filename=file, formatting_info=True)
     # 获取所有表格名字
     print(wb.sheet_names())
 
@@ -30,6 +30,17 @@ def read_excel():
     print(sheet1.cell(1, 0).value)
     print(sheet1.cell_value(1, 0))
     print(sheet1.row(1)[0].value)
+
+    print(sheet1.cell(1, 2).ctype)
+    date_value = xlrd.xldate_as_tuple(sheet1.cell_value(1, 2), wb.datemode)
+    print(date_value)
+    print(date(*date_value[:3]))
+    print(date(*date_value[:3]).strftime('%Y/%m/%d'))
+
+    print(sheet1.merged_cells)
+    print(sheet1.cell_value(1, 3))
+    print(sheet1.cell_value(4, 3))
+    print(sheet1.cell_value(6, 1))
 
 
 if __name__ == '__main__':
