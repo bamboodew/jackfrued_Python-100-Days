cef947ce235fcb8f94bbbf00293816e27eb4025f
bamboodew <mrbamboodew@gmail.com>
2019-11-09 18:41:14 +0800

    Python_Excel_Read

---
+++ src/Day011_Files_and_exceptions/08_ExcelRead.py
@@ -0,0 +1,36 @@
+import xlrd
+
+# from datetime import date, datetime
+
+file = 'test3.xls'
+
+
+def read_excel():
+    # 打开文件
+    wb = xlrd.open_workbook(filename=file)
+    # 获取所有表格名字
+    print(wb.sheet_names())
+
+    # 通过索引获取表格
+    sheet1 = wb.sheet_by_index(0)
+    # 通过名字获取表格
+    sheet2 = wb.sheet_by_name('年级')
+
+    print(sheet1, sheet2)
+    print(sheet1.name, sheet1.nrows, sheet1.ncols)
+
+    # 获取行内容
+    rows = sheet1.row_values(2)
+    # 获取列内容
+    cols = sheet1.col_values(3)
+    print(rows)
+    print(cols)
+
+    # 获取表格里的内容，三种方式
+    print(sheet1.cell(1, 0).value)
+    print(sheet1.cell_value(1, 0))
+    print(sheet1.row(1)[0].value)
+
+
+if __name__ == '__main__':
+    read_excel()
