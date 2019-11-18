e0c8907dd02ae08bb85856d7df8b3d7d7e1d6b39
bamboodew <mrbamboodew@gmail.com>
2019-11-18 20:14:25 +0800

    Inserting An Image

--- src/Day015_ImageAndOfficeDocumentProcessing/05_SimpleUsage_UsingNumberFormats.py
+++ src/Day015_ImageAndOfficeDocumentProcessing/05_SimpleUsage_UsingNumberFormats.py
@@ -1,6 +1,7 @@
 import datetime
 
 from openpyxl import Workbook
+from openpyxl.drawing.image import Image
 from openpyxl.utils import FORMULAE
 
 wb = Workbook()
@@ -9,6 +10,17 @@ ws['A1'] = datetime.datetime(2010, 7, 21)
 print(ws['A1'].number_format)
 
 ws["A2"] = "=SUM(1,1)"
-wb.save('formula.xlsx')
 
 print('HEX2DEC' in FORMULAE)
+
+ws.merge_cells('A3:D3')
+# ws.unmerge_cells('A3:D3')
+ws.merge_cells(start_row=4, start_column=1, end_row=5, end_column=5)
+# ws.unmerge_cells(start_row=4, start_column=1, end_row=5, end_column=5)
+
+# Inserting an image
+ws['A6'] = 'You should see logo below'
+img = Image('logo.png')
+ws.add_image(img, 'A7')
+
+wb.save('formula.xlsx')
